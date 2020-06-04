172a1f8c44664e2942a93166e99d76d0bd84adfa
SergeJohanns <serge.johanns@gmail.com>
2020-06-04 16:59:58 +0200

    Removed old code

--- main.hs
+++ main.hs
@@ -58,30 +58,5 @@ tokenize = tokenize' []
 
 -- Parse a JSON value
 
-{-
--- Grab all of the string up to but exluding the closing brace, and return the rest in the second element.
--- Deals with \ escape
-untilMatchingBrace' :: Int -> String -> String -> Maybe (String, String)
-untilMatchingBrace' 0 acc remains = Just ((reverse . tail) acc, remains) -- Slice of the closing brace
-untilMatchingBrace' level acc "" = Nothing
-untilMatchingBrace' level acc ('\\':remains) = untilMatchingBrace' level (next:'\\':acc) remains'
-    where (next:remains') = remains
-untilMatchingBrace' level acc ('{':remains) = untilMatchingBrace' (level + 1) ('{':acc) remains
-untilMatchingBrace' level acc ('}':remains) = untilMatchingBrace' (level - 1) ('}':acc) remains
-untilMatchingBrace' level acc ('"':remains) = skipQuoted remains >>= f
-    where f (quoted, unquoted) = untilMatchingBrace' level (reverse quoted ++ ('"':acc)) unquoted
-untilMatchingBrace' level acc (r:remains) = untilMatchingBrace' level (r:acc) remains
-
--- Preapply level and accumulator
-untilMatchingBrace :: String -> Maybe (String, String)
-untilMatchingBrace = untilMatchingBrace' 1 ""
-
-splitKVPairs :: String -> [(String, String)]
-splitKVPairs = undefined
-
-parse :: String -> Either String JsonValue
-parse = undefined
--}
-
 main = do
     print $ tokenize "{\"Hello there\":null, \"How is it going?\": 10.42}"
